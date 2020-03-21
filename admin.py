520c09637d42d0e19d4e063600cd593d0205a50b
Mirko Arena <mirko_arena@discovery.com>
2020-03-21 22:35:19 +0100

    Fixed Doctor model

--- admin.py
+++ admin.py
@@ -6,9 +6,15 @@ from staysafemed.models import (
 )
 
 
+class DoctorAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        return Doctor.objects.filter(is_staff=False)
+
+
 admin.site.register(IllnessStatus)
 admin.site.register(Hospital)
-admin.site.register(Doctor)
+admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(Patient)
 admin.site.register(HospitalHasPatient)
 admin.site.register(IllnessStatusHasPatient)
--- models/doctor.py
+++ models/doctor.py
@@ -1,15 +1,16 @@
-from django.db import models
+from django.contrib.auth.models import User
 
 
-class Doctor(models.Model):
-    name = models.CharField(verbose_name="Doctor Name", max_length=20)
-    surname = models.CharField(verbose_name="Doctor Surname", max_length=20)
-    password = models.CharField(max_length=40)
-    email = models.EmailField(verbose_name="Doctor Email Address", blank=True, null=True) # noqa
-    ssnid = models.CharField(verbose_name="Doctor Health System Id", max_length=30, unique=True) # noqa
+class Doctor(User):
+    class Meta:
+        proxy = True
 
     def __str__(self):
         return '{} {}'.format(
-            self.name,
-            self.surname
+            self.first_name,
+            self.last_name
         )
+
+    @property
+    def ssnid(self):
+        return self.username
