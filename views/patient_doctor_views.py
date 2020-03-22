400608d949a1e557926a8a14ac25527a4efc3dee
Mirko Cappai <mirko_cappai@discovery.com>
2020-03-22 20:38:58 +0100

    frontend template structure

--- views/patient_doctor_views.py
+++ views/patient_doctor_views.py
@@ -7,7 +7,7 @@ from staysafemed.models.patient import Patient
 class PatientsDoctorListView(generic.ListView):
     model = Patient
     context_object_name = 'patientsList'
-    template_name = 'doctor/patients_list.html'
+    template_name = 'doctor/dashboard.html'
 
     def get_queryset(self):
         return sorted(Patient.objects.all(), key=lambda p: p.lastIllnessDataScore, reverse=True)
