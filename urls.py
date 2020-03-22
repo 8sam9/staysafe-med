fcadea1140936309bea093ceaf3b0cb4fd29562c
Samuele Macchi <samuele_macchi@discovery.com>
2020-03-22 18:03:45 +0000

    fix view

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from staysafemed.views import *
 
 urlpatterns = [
-    path('patients/', PatientsDoctorView.as_view()),
-    path('patients/<str:otp>/', PatientDoctorDetailView.as_view()),
+    path('patients/', PatientsDoctorListView.as_view()),
+    path('patients/<int:id>/', PatientDoctorDetailView.as_view()),
     path('<str:otp>/', PatientIllnessDataView.as_view())
 ]
