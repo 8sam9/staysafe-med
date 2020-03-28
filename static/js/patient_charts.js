18d02ef0d1838c817ee0e020340f6a2c9ec76e53
Mirko Arena <mirko_arena@discovery.com>
2020-03-28 01:35:40 +0100

    Added some new custom params to chart and moved mew score chart on top - Doctor asked for this :)

--- static/js/patient_charts.js
+++ static/js/patient_charts.js
@@ -1,22 +1,27 @@
-function createChart(id,type,labels,datasets){
+function createChart({id, type, labels, datasets, yAxesStepSize, aspectRatio=2}){
 
     console.log(datasets);
     var ctx = document.getElementById(id).getContext('2d');
+    var chartOptions = {
+        aspectRatio: aspectRatio,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    };
+    if (yAxesStepSize) {
+        chartOptions.scales.yAxes[0].ticks.stepSize = yAxesStepSize;
+    }
     var myChart = new Chart(ctx, {
         type: type,
         data: {
             labels: labels,
             datasets: datasets
         },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+        options: chartOptions
     });
 
 };
